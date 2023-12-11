1859829a6b7ea9bd1c3f04f58a16a20fe180f60e
saicharankavuri <saicharankavuri01@gmail.com>
2023-12-11 08:17:52 -0500

    added visual regression test

--- cypress/e2e/FirstTest.cy.js
+++ cypress/e2e/FirstTest.cy.js
@@ -1,6 +1,10 @@
 describe('template spec', () => {
+
+
+  //this is the e2e test
   it('passes', () => {
-    cy.visit('http://138.197.36.40:3000')
+    cy.visit('http://138.197.36.40:3000/')
+    y.url().should('eq', 'http://138.197.36.40:3000/signin');
 
     cy.get('input[type="email"]').type('charan@gmail.com');
     cy.get('input[type="password"]').type('1234');
@@ -11,5 +15,16 @@ describe('template spec', () => {
     // Assert that the user is redirected to the home page or another expected page
     cy.url().should('eq', 'http://138.197.36.40:3000/');
 
+  })
+
+
+  //this is the Visual Regression test using APPLITOOLS AND CYPRESS
+  it('should look the same', () =>{
+    cy.eyesOpen({
+      appName: 'Budget',
+      testName: 'Hompage Check'
+    });
+    cy.eyesCheckWindow();
+    cy.eyesClone();
   })
 })
